c9addd485d30cea41e6d4662e366dde06d994d4d
zhangyemengren <624593758@qq.com>
2024-09-05 12:04:03 +0800

    update

--- recipes/ContentView.swift
+++ recipes/ContentView.swift
@@ -2,44 +2,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(alignment: .bottom) {
-            TabView {
-                Text("Home Screen")
-                    .tabItem {
-                        Image(systemName: "house.fill")
-                        Text("Home")
-                    }
-                
-                Text("Profile Screen")
-                    .tabItem {
-                        Image(systemName: "person.fill")
-                        Text("Profile")
-                    }
-            }
-            .zIndex(0)
-            HStack{
-                Button(action: {}) {
-                    ZStack {
-                        Circle()
-                            .fill(Color.yellow)
-                                                            .frame(width: 50, height: 50)
-                        Image(systemName: "plus")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30) // Adjust size here
-                            .foregroundColor(.black)
-                        
+        NavigationView{
+            ZStack(alignment: .bottom) {
+                TabView {
+                    Text("Home Screen")
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("Home")
+                        }
                     
-                    }
-                    .padding(.bottom, 0)
-                    
-                    
+                    Text("Profile Screen")
+                        .tabItem {
+                            Image(systemName: "person.fill")
+                            Text("Profile")
+                        }
                 }
-                .offset(y: -10) // Adjust vertical offset here
+                .zIndex(0)
+                HStack{
+                    NavigationLink(destination: AddPage()){
+                        ZStack {
+                            Circle()
+                                .fill(Color.yellow)
+                                .frame(width: 50, height: 50)
+                            Image(systemName: "plus")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) // Adjust size here
+                                .foregroundColor(.black)
+                            
+                            
+                        }
+                        .padding(.bottom, 0)
+                
+                    }
+                    .offset(y: -10)
+                   
+                }
+                .zIndex(1)
             }
-            .zIndex(1)
         }
-        
     }
 }
 
---
+++ recipes/pages/AddPage.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct AddPage:View {
+    var body: some View {
+        Text("添加页")
+            .navigationTitle("添加页")
+    }
+}
+
+#Preview {
+    AddPage()
+}
